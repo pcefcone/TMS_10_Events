4355f3d0f9b6bee113f1440c9f1b834dd4119183
Yaroslav <yaroslav.kerez@gmail.com>
2024-08-02 14:26:58 +0200

    Add project files.

---
+++ Operations.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_10_Events
+{
+    internal class Operations
+    {
+        private double variable1 = 0;
+        private double variable2 = 0;
+        private double res = 0;
+        public double result
+        {
+            get
+            {
+                return res;
+            }
+
+        }
+        public event Action<object> Addition;
+        public event Action<object> Subtraction;
+        public event Action<object> Multiplication;
+        public event Action<object> Division;
+        public event Action<object> Modulus;
+        public event Action<object> Exit;
+        public event Action Error;
+
+        public void Calculate()
+        {
+            variable1 = 0;  
+            variable2 = 0;
+        }
+        public void Calculation(double variable1, string operation, double variable2)
+        {
+            this.variable1 = variable1;
+            this.variable2 = variable2;
+            switch (operation)
+            {
+                case "+":
+                    if (Addition != null)
+                    {
+                        Addition(this);
+                    }
+                    break;
+                case "-":
+                    if (Subtraction != null)
+                    {
+                        Subtraction(this);
+                    }
+                    break;
+                case "*":
+                    if (Multiplication != null)
+                    {
+                        Multiplication(this);
+                    }
+                    break;
+                case "/":
+                    if (Division != null && variable2 != 0)
+                    {
+                        Division(this);
+                    }
+                    else
+                    {
+                        goto default;
+                    }
+                    break;
+                case "%":
+                    if (Modulus != null && variable2 != 0)
+                    {
+                        Modulus(this);
+                    }
+                    else
+                    {
+                        goto default;
+                    }
+                    break;
+                case "!":
+                    if (Exit != null)
+                    {
+                        Exit(this);
+                    }
+                    break;
+               
+                default:
+                    Error();
+                    break;
+            }
+        }
+        public void Add()
+        {
+            res = variable1 + variable2;
+        }
+        public void Sub()
+        {
+            res = variable1 - variable2;
+        }
+        public void Mult()
+        {
+            res = variable1 * variable2;
+        }
+        public void Div()
+        {
+            res = variable1 / variable2;
+        }
+        public void Mod()
+        {
+            res = variable1 % variable2;
+        }
+
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using TMS_10_Events;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Operations operations = new Operations();
+        operations.Addition += OperationAddition;
+        operations.Division += OperationDivision;
+        operations.Multiplication += OperationMultiplication;
+        operations.Subtraction += OperationSubstraction;
+        operations.Modulus += OperationModulus;
+        operations.Error += OperationError;
+        double variable1;
+        double variable2;
+        bool isTrue = true;
+
+        while (isTrue)
+        {
+            Console.WriteLine("Put first number: ");
+            bool trueVariable1 = double.TryParse(Console.ReadLine(), out variable1);
+
+            Console.WriteLine("Put second number: ");
+            bool trueVariable2 = double.TryParse(Console.ReadLine(), out variable2);
+
+            if (trueVariable1 && trueVariable1)
+            {
+                Console.WriteLine("Choose operation:" +
+                    "+ -> to addition," +
+                    "- -> to division," +
+                    "* -> to multiplication," +
+                    "/ -> to substraction," +
+                    "% -> to modulus," +
+                    "! -> to exit program" +
+                    "another symbol, letter or number -> to error=)");
+                string operation = Console.ReadLine();
+
+                operations.Calculation(variable1, operation, variable2);
+                
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+    }
+    private static void OperationAddition(object obj)
+    {
+        var operations = (Operations)obj;
+        operations.Add();
+        Console.WriteLine($"Addition result is: {operations.result}");
+    }
+    public static void OperationSubstraction(object obj)
+    {
+        var operations = (Operations)obj;
+        operations.Sub();
+        Console.WriteLine($"Substraction result is: {operations.result}");
+    }
+    public static void OperationDivision(object obj)
+    {
+        var operations = (Operations)obj;
+        operations.Div();
+        Console.WriteLine($"Division result is: {operations.result}");
+    }
+    public static void OperationMultiplication(object obj)
+    {
+        var operations = (Operations)obj;
+        operations.Mult();
+        Console.WriteLine($"Multiplication result is: {operations.result}");
+    }
+    public static void OperationModulus(object obj)
+    {
+        var operations = (Operations)obj;
+        operations.Mod();
+        Console.WriteLine($"Modulus result is: {operations.result}");
+    }
+    public static void OperationError()
+    {      
+        Console.WriteLine("Error!");
+    }
+}
